e40cb43b3eed49998dc1ab91b5e56eda10b41456
Démiurge <64045692+thepoulet153@users.noreply.github.com>
2024-06-17 14:15:21 +0200

    Update DATAJson.py
    gère mieux les questions à choix multiples

--- modules/DATAJson.py
+++ modules/DATAJson.py
@@ -1,4 +1,6 @@
 from colorama import Fore, Style
+from rich.console import Console
+
 
 class DATAJson:
     
@@ -7,10 +9,11 @@ class DATAJson:
     
     def get_result(self):
         if len(self.ANSWERS) > 1:
-            user_answer = input("Veuillez choisir les bonnes réponses : ").split(" ")
+            user_answer = input("Veuillez choisir les bonnes réponses (séparées par des espaces) : ").split()
         else:
-            user_answer = input("Veuillez choisir la bonne réponse : ").split(" ")
+            user_answer = input("Veuillez choisir la bonne réponse : ").split()
             if len(user_answer) > 1:
+                console.print("[bold red]Vous ne devez choisir qu'une seule réponse.[/bold red]")
                 return False
         
         if self.is_good_a_answer(user_answer):
