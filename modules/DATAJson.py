0eaf882a9400787f0adec1be15a8841393d58d6b
Démiurge <64045692+thepoulet153@users.noreply.github.com>
2024-06-17 15:04:23 +0200

    Update DATAJson.py
    Meilleur gestion des questions à choix multiples

--- modules/DATAJson.py
+++ modules/DATAJson.py
@@ -1,4 +1,7 @@
 from colorama import Fore, Style
+from rich.console import Console
+
+console = Console()
 
 class DATAJson:
     
@@ -7,10 +10,11 @@ class DATAJson:
     
     def get_result(self):
         if len(self.ANSWERS) > 1:
-            user_answer = input("Veuillez choisir les bonnes réponses : ").split(" ")
+            user_answer = input("Veuillez choisir les bonnes réponses (séparées par des espaces) : ").split()
         else:
-            user_answer = input("Veuillez choisir la bonne réponse : ").split(" ")
+            user_answer = input("Veuillez choisir la bonne réponse : ").split()
             if len(user_answer) > 1:
+                console.print("[bold red]Vous ne devez choisir qu'une seule réponse.[/bold red]")
                 return False
         
         if self.is_good_a_answer(user_answer):
@@ -26,11 +30,11 @@ class DATAJson:
         for answer in user_answer:
             try:
                 if int(answer) in self.ANSWERS and int(answer) not in pass_answer:
-                    count +=1
+                    count += 1
                     pass_answer.append(int(answer))
                     
-            except:
+            except ValueError:
                 print(f"{Style.DIM}{Fore.RED}MERCI de spécifier une valeur correcte.{Style.RESET_ALL}")
-                self.get_result()
+                return self.get_result()
                 
         return count == len(self.ANSWERS)
